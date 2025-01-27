612924bf33fe0b27fd2b58a9bbcbc80d4add6cb3
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-01-27 15:22:53 +0530

    created backend queries for user model for signup and signin

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,27 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const {setUser,getUser} = require('../services/auth');
+
+
+const getUser = async (req,res) => {
+  const user = req.body;
+  const {email , password}  = user;
+  const isSignup = await User.findOne({ email: email });
+  
+  if(!isSignup){
+    console.log('user does not exist');
+    return res.json({msg:"use is not exist signup"});
+  }
+
+  const isValid = bcrypt.compareSync(password, isSignup.password);
+
+  if(isValid){
+    return res.status(200).json({msg:"user is logged in"});
+  } else {
+    const token = setUser(user);
+    return res.status(500).json({msg:"server error"});
+  }
+};
 
 const createUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -10,9 +33,15 @@ const createUser = async (req, res) => {
     return res.json({ msg: "User is already signed up" });
   }
   else {
-    await User.create({ name, email, password });
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    await User.create({ name:name, email:email, password:hashedPassword });
     res.status(200).json({msg:"you are signed up"});
   }
 }
 
-module.exports = createUser;
+module.exports = {
+  getUser,
+  createUser
+}
