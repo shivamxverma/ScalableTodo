1f6f4921c7536f401ecc45d6546a3ba5fce93f2f
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-01-27 09:50:59 +0530

    added user model for signup authentication

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
+// const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const UserSchema = new mongoose.Schema({
-  name : String,
-  email : String,
-  password : String,
-  todos : [{
-    type : mongoose.Schema.Types.ObjectId,
-    ref : 'todo'
+  name: String,
+  email: String,
+  password: String,
+  todos: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'todo'
   }]
-})
+});
+
+// UserSchema.plugin(AutoIncrement, { inc_field: 'id' });
 
 const user = mongoose.model('user',UserSchema);
 
