217983d2ee480906da0065703a244ed92b879312
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-01-27 09:50:24 +0530

    modified todo model for storing todo for each users

--- backend/models/todo.js
+++ backend/models/todo.js
@@ -4,6 +4,19 @@ const TodoSchema = new mongoose.Schema({
   todo : String,
   description : String,
   isDone : Boolean,
+  difficulty : Number,
+  date : {
+    type : Date,
+    default : Date.now
+  },  
+  time : {
+    type : Number,
+    default : 0
+  },
+  user : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'user'
+  }
 });
 
 const todo = mongoose.model('todos',TodoSchema);
