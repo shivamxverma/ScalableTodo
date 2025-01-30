335be03a94873b448e54a0d19d40397bc536540e
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-01-30 23:13:31 +0530

    added cookies logic

--- backend/controllers/todo.js
+++ backend/controllers/todo.js
@@ -1,52 +1,92 @@
-const Todo = require('../models/todo');
+const Todo = require("../models/todo");
+const mongoose = require("mongoose");
 
 const getTodos = async (_, res) => {
+  if (!req.user || !req.user.id) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
   try {
-    const todos = await Todo.find();
+    const todos = await Todo.find({ userId: req.user.id });
     res.status(200).json(todos);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(500).json({ message: "Server error: " + error.message });
   }
-}
+};
 
 const createTodo = async (req, res) => {
-  const todo = req.body;
-  const newTodo = new Todo({
-    ...todo
-  });
+  const {todo,description,idDone, difficulty, date ,time} = req.body;
+  
+  if (!req.user || !req.user.id) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  
+  const newTodo = new Todo({ 
+    todo, 
+    description, 
+    isDone: false, 
+    difficulty, 
+    date, 
+    time,
+    user: req.user.id
+   });
 
   try {
     await newTodo.save();
     res.status(201).json(newTodo);
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    res.status(400).json({ message: "Error creating todo: " + error.message });
   }
-}
+};
 
 const updateTodo = async (req, res) => {
   const { id } = req.params;
   const todo = req.body;
 
-  if (!Todo.isValid(id)) return res.status(404).send('No todo with that id');
+  // console.log("hii i am in update todo");
+
+  // ✅ Check if ID is a valid MongoDB ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: "Invalid todo ID" });
+  }
 
   const updatedTodo = await Todo.findByIdAndUpdate(id, todo, { new: true });
 
+  if (!updatedTodo) {
+    return res.status(404).json({ message: "No todo found with this ID" });
+  }
+
+  await todo.save();
   res.json(updatedTodo);
-}
+};
 
 const deleteTodo = async (req, res) => {
   const { id } = req.params;
 
-  if (!Todo.isValid(id)) return res.status(404).send('No todo with that id');
+  console.log("hii i am in delete todo");
 
-  await Todo.findByIdAndRemove(id);
+  // ✅ Check if ID is a valid MongoDB ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: "Invalid todo ID" });
+  }
 
-  res.json({ message: 'Todo deleted successfully' });
-}
+  const todo = await Todo.findById(id);
+
+  if (!todo) {
+    return res.status(404).json({ message: "No todo found with this ID" });
+  }
+
+  if (todo.userId.toString() !== req.user.id) {
+    return res.status(403).json({ message: "Not authorized to delete this todo" });
+  }
+
+  await Todo.findByIdAndDelete(id);
+
+  res.json({ message: "Todo deleted successfully" });
+};
 
 module.exports = {
   getTodos,
   createTodo,
   updateTodo,
-  deleteTodo
-}
+  deleteTodo,
+};
